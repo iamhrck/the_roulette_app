db2e08700b065948236ee867b9584667617f7b6b
iamhrck <90491511+iamhrck@users.noreply.github.com>
2023-12-13 21:11:50 +0900

    add constants

---
+++ lib/shared/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color black = Color(0xFF2D2D2D);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color gray = Color(0xFFF2F2F2);
+  static const Color blue = Color(0xFF4Eb4F9);
+  static const Color lightBlue = Color(0xFF88E0FF);
+}
---
+++ lib/shared/constants/app_text_style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  AppTextStyle._();
+
+  static const TextStyle headline3 =
+      TextStyle(fontSize: 24.0, fontWeight: FontWeight.w700);
+  static const TextStyle headline4 =
+      TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500);
+  static const TextStyle bodyText = TextStyle(fontSize: 18.0);
+}
